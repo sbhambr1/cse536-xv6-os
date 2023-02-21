4ed3c426cf0c9a4f295b80b48652f1c2b20dd295
Siddhant Bhambri <sbhambr1@asu.edu>
2023-02-21 10:57:55 -0700

    switched to lab2-memory

---
+++ bootloader/elf.d
@@ -0,0 +1,5 @@
+bootloader/elf.o: bootloader/elf.c bootloader/types.h bootloader/param.h \
+ bootloader/memlayout.h bootloader/riscv.h bootloader/defs.h \
+ /home/local/ASUAD/sbhambr1/Desktop/CourseProjects/cse536-advOS/cse536-release/install/riscv-gnu-toolchain/build/lib/gcc/riscv64-unknown-elf/12.2.0/include/stddef.h \
+ bootloader/buf.h bootloader/elf.h \
+ /home/local/ASUAD/sbhambr1/Desktop/CourseProjects/cse536-advOS/cse536-release/install/riscv-gnu-toolchain/build/lib/gcc/riscv64-unknown-elf/12.2.0/include/stdbool.h
