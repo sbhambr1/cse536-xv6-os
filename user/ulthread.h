f46e537ca087066c69eff28b9a1fcd073d857132
Siddhant Bhambri <sbhambr1@asu.edu>
2023-03-22 12:33:25 -0700

    struct for ulthread added

--- user/ulthread.h
+++ user/ulthread.h
@@ -5,6 +5,16 @@
 
 #define MAXULTHREADS 100
 
+// create a data structure to hold the thread information
+struct ulthread {
+  int tid;
+  int priority;
+  enum ulthread_state state;
+  uint64 ra;
+  uint64 sp;
+  uint64 args[4];
+};
+
 enum ulthread_state {
   FREE,
   RUNNABLE,
