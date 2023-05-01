8bedf1c82af3757fce1a79eb3e192f8cc7edf806
Siddhant Bhambri <sbhambr1@asu.edu>
2023-05-01 14:13:32 -0700

    csrr csrw working

--- kernel/trap-and-emulate.c
+++ kernel/trap-and-emulate.c
@@ -7,6 +7,8 @@
 #include "defs.h"
 #include "stdbool.h"
 
+struct vm_virtual_state vms;
+
 // Program to convert binary to hex
 long int binaryToHex(long int n) {
     int remainder; 
@@ -82,20 +84,17 @@ struct vm_virtual_state {
     struct vm_reg mtval2;
 
     // Process trapframe registers
-    struct vm_reg s0;
-    struct vm_reg s1;
-    struct vm_reg a0;
-    struct vm_reg a1;
-    struct vm_reg a2;
-    struct vm_reg a3;
-    struct vm_reg a4;
-    struct vm_reg a5;
+    struct vm_reg sscratch;
+    struct vm_reg sepc;
+    struct vm_reg scause;
+    struct vm_reg stval;
+    struct vm_reg sip;
 
     // Current execution privilege level
     int priv; // 0 = U, 1 = S, 2 = M
 };
 
-void handle_sret(struct vm_virtual_state *vms){
+void handle_sret(){
     printf("handle_sret\n");
     // uint32 mstatus = r_mstatus(); // read mstatus register
     // uint32 spp = (mstatus >> 8) & 0x1; // get the previous privilege level (spp)
@@ -118,41 +117,40 @@ void handle_sret(struct vm_virtual_state *vms){
     // asm volatile("sret");
 }
 
-void handle_mret(struct vm_virtual_state *vms, struct proc *p){
+void handle_mret(struct proc *p){
     printf("handle_mret\n");
 
-    unsigned long x = vms->mstatus.val;
-    x &= ~MSTATUS_MPP_MASK;
-    x |= MSTATUS_MPP_S;
-    vms->mstatus.val = x;
+    unsigned long x = vms.regs[19].val;
 
-    vms->mepc.val = p->trapframe->epc;
+    printf("x: %x\n", x);
 
-    printf("epc: %x\n", p->trapframe->epc);
+    unsigned long int mpp = (x >> 11) & 0x1; // get the previous privilege level (mpp)
 
-    unsigned long int mstatus = vms->mstatus.val; // read mstatus register
-    unsigned long int mpp = (mstatus >> 11) & 0x1; // get the previous privilege level (mpp)
-    // set the interrupt enable bit (MIE) to mpie
-    mstatus &= ~MSTATUS_MIE; // clear MIE bit
-    vms->priv = mpp ? 1 : 0; // set the current privilege level (priv) to mpp
-    // set mpie to 1
-    mstatus |= 1 << 0x7; // set MIE bit to mpie
-    // set MPRV to 0
-    mstatus &= ~(1 << 0x17); // clear MPRV bit
+    x &= ~MSTATUS_MPP_MASK; // clear MPP bits
+  
+    unsigned long int mpie = (x >> 7) & 0x1; // get the previous interrupt enable bit (mpie)
+    x |= mpie << 3; // set MIE bit to MPIE
 
-    vms->mstatus.val = mstatus; // write mstatus register
+    x &= (1 << 0x7); // set MPIE bit to 1
 
-    p->trapframe->epc = vms->mtvec.val; // set the program count to the value of mtvec
-    
-    printf("epc: %x\n", p->trapframe->epc);
+    x &= ~(1 << 0x17); // clear MPRV bit
 
-    p->trapframe->epc = vms->mepc.val; // set the program count to the value of mepc
+    vms.priv = mpp ? 1 : 0; // set the current privilege level (priv) to mpp
+
+    vms.mstatus.val = x; // write mstatus register
+
+    p->trapframe->epc = vms.mepc.val; // set the program count to the value of mepc
+
+    printf("x: %x\n", vms.mstatus.val);
+    printf("mepc: %x\n", vms.mepc.val);
+
+    setkilled(p);
 
 }
 
-void handle_ecall(struct vm_virtual_state *vms){
+void handle_ecall(){
     printf("handle_ecall\n");
-    // printf("mstatus\n", vms->mstatus.val);
+    // printf("mstatus\n", vms.mstatus.val);
     // // TODO: Implement this function
     // // load syscall code from scause register
     // uint32 code = r_scause() & 0xf;
@@ -169,8 +167,8 @@ void handle_ecall(struct vm_virtual_state *vms){
     //     uint32 pc = r_pc();
     //     w_sepc(pc);
     //     // raise privilege level to supervisor mode
-    //     vms->priv = 1;
-    //     vms->mstatus.val |= vms->mstatus.val | (1 << 11) | (0 << 12);
+    //     vms.priv = 1;
+    //     vms.mstatus.val |= vms.mstatus.val | (1 << 11) | (0 << 12);
     //     // jump to stvec
     //     uint32 stvec = r_stvec();
     //     w_pc(stvec);
@@ -186,8 +184,8 @@ void handle_ecall(struct vm_virtual_state *vms){
     //     uint32 pc = r_pc();
     //     w_mepc(pc);
     //     // raise privilege level to machine mode
-    //     vms->priv = 2;
-    //     vms->mstatus.val |= vms->mstatus.val | (1 << 11) | (1 << 12);
+    //     vms.priv = 2;
+    //     vms.mstatus.val |= vms.mstatus.val | (1 << 11) | (1 << 12);
     //     // jump to mtvec
     //     uint32 mtvec = r_mtvec();
     //     w_pc(mtvec);
@@ -199,7 +197,7 @@ void handle_ecall(struct vm_virtual_state *vms){
     // }
 }
 
-void handle_csrr(struct vm_virtual_state *vms, unsigned int rs1, unsigned int rd, unsigned int upper){
+void handle_csrr(struct proc *p, unsigned int rs1, unsigned int rd, unsigned int upper){
     
     /* The CSRRS (Atomic Read and Set Bits in CSR) instruction reads the value of the CSR, zero-extends the value to XLEN bits, 
     and writes it to integer register rd
@@ -210,22 +208,28 @@ void handle_csrr(struct vm_virtual_state *vms, unsigned int rs1, unsigned int rd
     The assembler pseudoinstruction to read a CSR, CSRR rd, csr , is encoded as CSRRS rd, csr, x0 */
 
     uint32 value = 0;
-    struct proc *p = myproc();
     
     // iterate through the vms csr array to find the csr
-    for(int i = 0; i < 35; i++){
-        if(vms->regs[i].code == upper){
-            value = vms->regs[i].val;
-            break;
+    for(int i = 0; i < 39; i++){
+        if(vms.regs[i].code == upper){
+            if(vms.priv >= vms.regs[i].mode){
+                value = vms.regs[i].val;
+                uint64* rd_p = &(p->trapframe->ra) + rd - 1;
+                *rd_p = value;    
+                printf("value: %x\n", value);
+            }
+            else{
+                setkilled(p);
+            }
+        break;
         }
     }
 
-    p->trapframe->a1 = value;
     p->trapframe->epc += 4;
     
 }
 
-void handle_csrw(struct vm_virtual_state *vms, unsigned int rs1, unsigned int rd, unsigned int upper){
+void handle_csrw(struct proc *p, unsigned int rs1, unsigned int rd, unsigned int upper){
     
     /* The CSRRW (Atomic Read/Write CSR) instruction atomically swaps values in the CSRs and integer registers
     CSRRW reads the old value of the CSR, zero-extends the value to XLEN bits, then writes it to integer register rd
@@ -233,22 +237,18 @@ void handle_csrw(struct vm_virtual_state *vms, unsigned int rs1, unsigned int rd
     The assembler pseudoinstruction to write a CSR, CSRW csr, rs1 , is encoded as CSRRW x0, csr, rs1 , 
     while CSRWI csr, uimm , is encoded as CSRRWI x0, csr, uimm . */
 
-    uint32 value = 0;
-    struct proc *p = myproc();
-
     // iterate through the vms csr array to find the csr
-    for(int i = 0; i < 35; i++){
-        if(vms->regs[i].code == upper){
-            value = vms->regs[i].val;
-            break;
+    for(int i = 0; i < 39; i++){
+        if(vms.regs[i].code == upper){
+            if(vms.priv >= vms.regs[i].mode){
+                uint64* rs1_p= &(p->trapframe->ra) + rs1 - 1;
+                vms.regs[i].val = *rs1_p;
+            }
+            else{
+                setkilled(p);
+            }
+        break;
         }
-    }
-
-    for(int i = 0; i < 35; i++){
-        if(vms->regs[i].code == rd){
-            vms->regs[i].val = value;
-            break;
-        }  
     }
 
     p->trapframe->epc += 4;
@@ -257,8 +257,6 @@ void handle_csrw(struct vm_virtual_state *vms, unsigned int rs1, unsigned int rd
 
 void trap_and_emulate(void) {
     /* Comes here when a VM tries to execute a supervisor instruction. */
-    struct vm_virtual_state *vms = (struct vm_virtual_state *)kalloc();
-    memset(vms, 0, sizeof(struct vm_virtual_state));
 
     /* Retrieve all required values from the instruction */
     uint64 addr     = 0;
@@ -299,95 +297,99 @@ void trap_and_emulate(void) {
                 addr_p, op, rd, funct3, rs1, upper);
 
     if(funct3 == 0x1){
-        handle_csrw(vms, rs1 = rs1, rd = rd, upper = upper);
+        handle_csrw(p = p, rs1 = rs1, rd = rd, upper = upper);
     }
     else if(funct3 == 0x2){
-        handle_csrr(vms, rs1 = rs1, rd = rd, upper = upper);
+        handle_csrr(p = p, rs1 = rs1, rd = rd, upper = upper);
     }
     else if(funct3 == 0x0){
         if(upper == 0){
-            handle_ecall(vms);
+            handle_ecall();
             setkilled(p);
         } else if(upper == 0x102){
-            handle_sret(vms);
+            handle_sret();
             setkilled(p);
         } else if(upper == 0x302){
-            handle_mret(vms, p);
-            setkilled(p);
+            handle_mret(p = p);
         }
     }
     else{
         printf("trap_and_emulate: invalid instruction\n");
         setkilled(p);
     }
+
+    /* Clone the VM process' page tables
+    Unmap memory regions that the PMP says should not be accessible in U/S-mode.
+    The only PMP variant you must support is the TOR one you supported in assignment #1.
+    If the VM tries to access PMP protected memory region, it should raise a page fault
+    Then, you simply kill the VM. */
+
 }
 
 void trap_and_emulate_init(void) {
     /* Create and initialize all state for the VM */
-    struct vm_virtual_state *vms = (struct vm_virtual_state *)kalloc();
 
     // User trap setup
-    vms->regs[0]  = vms->ustatus = (struct vm_reg){.code = 0x000, .mode = 0, .val = 0};
-    vms->regs[1]  = vms->uie = (struct vm_reg){.code = 0x004, .mode = 0, .val = 0};
-    vms->regs[2]  = vms->utvec = (struct vm_reg){.code = 0x005, .mode = 0, .val = 0};
+    vms.regs[0]  = vms.ustatus = (struct vm_reg){.code = 0x000, .mode = 0, .val = 0};
+    vms.regs[1]  = vms.uie = (struct vm_reg){.code = 0x004, .mode = 0, .val = 0};
+    vms.regs[2]  = vms.utvec = (struct vm_reg){.code = 0x005, .mode = 0, .val = 0};
 
     // User trap handling
-    vms->regs[3]  = vms->uscratch = (struct vm_reg){.code = 0x040, .mode = 0, .val = 0};
-    vms->regs[4]  = vms->uepc = (struct vm_reg){.code = 0x041, .mode = 0, .val = 0};
-    vms->regs[5]  = vms->ucause = (struct vm_reg){.code = 0x042, .mode = 0, .val = 0};
-    vms->regs[6]  = vms->utval = (struct vm_reg){.code = 0x043, .mode = 0, .val = 0};
-    vms->regs[7]  = vms->uip = (struct vm_reg){.code = 0x044, .mode = 0, .val = 0};
+    vms.regs[3]  = vms.uscratch = (struct vm_reg){.code = 0x040, .mode = 0, .val = 0};
+    vms.regs[4]  = vms.uepc = (struct vm_reg){.code = 0x041, .mode = 0, .val = 0};
+    vms.regs[5]  = vms.ucause = (struct vm_reg){.code = 0x042, .mode = 0, .val = 0};
+    vms.regs[6]  = vms.utval = (struct vm_reg){.code = 0x043, .mode = 0, .val = 0};
+    vms.regs[7]  = vms.uip = (struct vm_reg){.code = 0x044, .mode = 0, .val = 0};
 
     // Supervisor trap setup
-    vms->regs[8]  = vms->sstatus = (struct vm_reg){.code = 0x100, .mode = 1, .val = 0};
-    vms->regs[9]  = vms->sedeleg = (struct vm_reg){.code = 0x102, .mode = 1, .val = 0};
-    vms->regs[10] = vms->sideleg = (struct vm_reg){.code = 0x103, .mode = 1, .val = 0};
-    vms->regs[11] = vms->sie = (struct vm_reg){.code = 0x104, .mode = 1, .val = 0};
-    vms->regs[12] = vms->stvec = (struct vm_reg){.code = 0x105, .mode = 1, .val = 0};
-    vms->regs[13] = vms->scounteren = (struct vm_reg){.code = 0x106, .mode = 1, .val = 0};
+    vms.regs[8]  = vms.sstatus = (struct vm_reg){.code = 0x100, .mode = 1, .val = 0};
+    vms.regs[9]  = vms.sedeleg = (struct vm_reg){.code = 0x102, .mode = 1, .val = 0};
+    vms.regs[10] = vms.sideleg = (struct vm_reg){.code = 0x103, .mode = 1, .val = 0};
+    vms.regs[11] = vms.sie = (struct vm_reg){.code = 0x104, .mode = 1, .val = 0};
+    vms.regs[12] = vms.stvec = (struct vm_reg){.code = 0x105, .mode = 1, .val = 0};
+    vms.regs[13] = vms.scounteren = (struct vm_reg){.code = 0x106, .mode = 1, .val = 0};
 
     // Supervisor page table register
-    vms->regs[14] = vms->satp = (struct vm_reg){.code = 0x180, .mode = 1, .val = 0};
+    vms.regs[14] = vms.satp = (struct vm_reg){.code = 0x180, .mode = 1, .val = 0};
+
+    // add supervisor trap handlin registers
 
     // Machine information registers
-    vms->regs[15] = vms->mvendorid = (struct vm_reg){.code = 0xF11, .mode = 2, .val = 637365353336};
-    vms->regs[16] = vms->marchid = (struct vm_reg){.code = 0xF12, .mode = 2, .val = 0};
-    vms->regs[17] = vms->mimpid = (struct vm_reg){.code = 0xF13, .mode = 2, .val = 0};
-    vms->regs[18] = vms->mhartid = (struct vm_reg){.code = 0xF14, .mode = 2, .val = 0};
+    vms.regs[15] = vms.mvendorid = (struct vm_reg){.code = 0xF11, .mode = 2, .val = 637365353336};
+    vms.regs[16] = vms.marchid = (struct vm_reg){.code = 0xF12, .mode = 2, .val = 0};
+    vms.regs[17] = vms.mimpid = (struct vm_reg){.code = 0xF13, .mode = 2, .val = 0};
+    vms.regs[18] = vms.mhartid = (struct vm_reg){.code = 0xF14, .mode = 2, .val = 0};
 
     // Machine trap setup registers
-    vms->regs[19] = vms->mstatus = (struct vm_reg){.code = 0x300, .mode = 2, .val = 0};
-    vms->regs[20] = vms->misa = (struct vm_reg){.code = 0x301, .mode = 2, .val = 0};
-    vms->regs[21] = vms->medeleg = (struct vm_reg){.code = 0x302, .mode = 2, .val = 0};
-    vms->regs[22] = vms->mideleg = (struct vm_reg){.code = 0x303, .mode = 2, .val = 0};
-    vms->regs[23] = vms->mie = (struct vm_reg){.code = 0x304, .mode = 2, .val = 0};
-    vms->regs[24] = vms->mtvec = (struct vm_reg){.code = 0x305, .mode = 2, .val = 0};
-    vms->regs[25] = vms->mcounteren = (struct vm_reg){.code = 0x306, .mode = 2, .val = 0};
-    vms->regs[26] = vms->mstatush = (struct vm_reg){.code = 0x310, .mode = 2, .val = 0};
+    vms.regs[19] = vms.mstatus = (struct vm_reg){.code = 0x300, .mode = 2, .val = 0};
+    vms.regs[20] = vms.misa = (struct vm_reg){.code = 0x301, .mode = 2, .val = 0};
+    vms.regs[21] = vms.medeleg = (struct vm_reg){.code = 0x302, .mode = 2, .val = 0};
+    vms.regs[22] = vms.mideleg = (struct vm_reg){.code = 0x303, .mode = 2, .val = 0};
+    vms.regs[23] = vms.mie = (struct vm_reg){.code = 0x304, .mode = 2, .val = 0};
+    vms.regs[24] = vms.mtvec = (struct vm_reg){.code = 0x305, .mode = 2, .val = 0};
+    vms.regs[25] = vms.mcounteren = (struct vm_reg){.code = 0x306, .mode = 2, .val = 0};
+    vms.regs[26] = vms.mstatush = (struct vm_reg){.code = 0x310, .mode = 2, .val = 0};
 
     // Machine trap handling registers
-    vms->regs[27] = vms->mscratch = (struct vm_reg){.code = 0x340, .mode = 2, .val = 0};
-    vms->regs[28] = vms->mepc = (struct vm_reg){.code = 0x341, .mode = 2, .val = 0};
-    vms->regs[29] = vms->mcause = (struct vm_reg){.code = 0x342, .mode = 2, .val = 0};
-    vms->regs[30] = vms->mtval = (struct vm_reg){.code = 0x343, .mode = 2, .val = 0};
-    vms->regs[31] = vms->mip = (struct vm_reg){.code = 0x344, .mode = 2, .val = 0};
-    vms->regs[32] = vms->mtinst = (struct vm_reg){.code = 0x34A, .mode = 2, .val = 0};
-    vms->regs[33] = vms->mtval2 = (struct vm_reg){.code = 0x34B, .mode = 2, .val = 0};
+    vms.regs[27] = vms.mscratch = (struct vm_reg){.code = 0x340, .mode = 2, .val = 0};
+    vms.regs[28] = vms.mepc = (struct vm_reg){.code = 0x341, .mode = 2, .val = 0};
+    vms.regs[29] = vms.mcause = (struct vm_reg){.code = 0x342, .mode = 2, .val = 0};
+    vms.regs[30] = vms.mtval = (struct vm_reg){.code = 0x343, .mode = 2, .val = 0};
+    vms.regs[31] = vms.mip = (struct vm_reg){.code = 0x344, .mode = 2, .val = 0};
+    vms.regs[32] = vms.mtinst = (struct vm_reg){.code = 0x34A, .mode = 2, .val = 0};
+    vms.regs[33] = vms.mtval2 = (struct vm_reg){.code = 0x34B, .mode = 2, .val = 0};
 
     // Process trapframe registers
-    vms->regs[34] = vms->s0 = (struct vm_reg){.code = 0x000, .mode = 0, .val = 0};
-    vms->regs[35] = vms->s1 = (struct vm_reg){.code = 0x001, .mode = 0, .val = 0};
-    vms->regs[36] = vms->a0 = (struct vm_reg){.code = 0x010, .mode = 0, .val = 0};
-    vms->regs[37] = vms->a1 = (struct vm_reg){.code = 0x011, .mode = 0, .val = 0};
-    vms->regs[38] = vms->a2 = (struct vm_reg){.code = 0x100, .mode = 0, .val = 0};
-    vms->regs[39] = vms->a3 = (struct vm_reg){.code = 0x101, .mode = 0, .val = 0};
-    vms->regs[40] = vms->a4 = (struct vm_reg){.code = 0x110, .mode = 0, .val = 0};
-    vms->regs[41] = vms->a5 = (struct vm_reg){.code = 0x111, .mode = 0, .val = 0};
+    vms.regs[34] = vms.sscratch = (struct vm_reg){.code = 0x140, .mode = 1, .val = 0};
+    vms.regs[35] = vms.sepc = (struct vm_reg){.code = 0x141, .mode = 1, .val = 0};
+    vms.regs[36] = vms.scause = (struct vm_reg){.code = 0x142, .mode = 1, .val = 0};
+    vms.regs[37] = vms.stval = (struct vm_reg){.code = 0x143, .mode = 1, .val = 0};
+    vms.regs[38] = vms.sip = (struct vm_reg){.code = 0x144, .mode = 1, .val = 0};
 
     // Current execution privilege level
-    vms->priv = 2; // 0 = U, 1 = S, 2 = M
+    vms.priv = 2; // 0 = U, 1 = S, 2 = M
 
     // boot the VM in the M mode by setting 11th and 12th bit of mstatus to 1
-    vms->mstatus.val = vms->mstatus.val | (1 << 11) | (1 << 12);
+    vms.mstatus.val = vms.mstatus.val | (1 << 11) | (1 << 12);
     
 }
