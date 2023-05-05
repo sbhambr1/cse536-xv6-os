5817c4250591b1f4b1c9ce46a9488fb2eb72964a
Siddhant Bhambri <sbhambr1@asu.edu>
2023-05-04 22:55:32 -0700

    start.c unchanged

--- vm/start.c
+++ vm/start.c
@@ -31,7 +31,7 @@ start()
   // set M Previous Privilege mode to Supervisor, for mret.
   unsigned long x = r_mstatus();
   x &= ~MSTATUS_MPP_MASK;
-  x |= MSTATUS_MPP_U;
+  x |= MSTATUS_MPP_S;
   w_mstatus(x);
 
   // disable paging for now.
